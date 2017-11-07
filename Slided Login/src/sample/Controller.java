41abfa327434f84e78ce34d52c66b7100e846c7d
Ernesto Ramiro <atomms@gmail.com>
2017-11-07 13:10:10 +0100

    initial

---
+++ Slided Login/src/sample/Controller.java
@@ -0,0 +1,94 @@
+package sample;
+
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Pane pane1;
+
+    @FXML
+    private Pane pane2;
+
+    @FXML
+    private Pane pane3;
+
+    @FXML
+    private Pane pane4;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        pane1.setStyle("-fx-background-image: url(\"/sample/1.jpg\")");
+        pane2.setStyle("-fx-background-image: url(\"/sample/2.jpg\")");
+        pane3.setStyle("-fx-background-image: url(\"/sample/3.jpg\")");
+        pane4.setStyle("-fx-background-image: url(\"/sample/4.jpg\")");
+
+
+        backgroundAnimation();
+
+    }
+
+    private void backgroundAnimation() {
+
+        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(3),pane4);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        fadeTransition.setOnFinished(event -> {
+
+            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(3),pane3);
+            fadeTransition1.setFromValue(1);
+            fadeTransition1.setToValue(0);
+            fadeTransition1.play();
+
+            fadeTransition1.setOnFinished(event1 -> {
+                FadeTransition fadeTransition2=new FadeTransition(Duration.seconds(3),pane2);
+                fadeTransition2.setFromValue(1);
+                fadeTransition2.setToValue(0);
+                fadeTransition2.play();
+
+                fadeTransition2.setOnFinished(event2 -> {
+
+                   FadeTransition fadeTransition0 =new FadeTransition(Duration.seconds(3),pane2);
+                    fadeTransition0.setToValue(1);
+                    fadeTransition0.setFromValue(0);
+                    fadeTransition0.play();
+
+                    fadeTransition0.setOnFinished(event3 -> {
+
+                        FadeTransition fadeTransition11 =new FadeTransition(Duration.seconds(3),pane3);
+                        fadeTransition11.setToValue(1);
+                        fadeTransition11.setFromValue(0);
+                        fadeTransition11.play();
+
+                        fadeTransition11.setOnFinished(event4 -> {
+
+                            FadeTransition fadeTransition22 =new FadeTransition(Duration.seconds(3),pane4);
+                            fadeTransition22.setToValue(1);
+                            fadeTransition22.setFromValue(0);
+                            fadeTransition22.play();
+
+                            fadeTransition22.setOnFinished(event5 -> {
+                                backgroundAnimation();
+                            });
+
+                        });
+
+                    });
+
+                });
+            });
+
+        });
+
+    }
+}
---
+++ Slided Login/src/sample/Main.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("sample.fxml"));
+        AnchorPane pane=loader.load();
+        Scene scene=new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
